63dee3fb06f1e0a37c409ed49f5a3695ee846419
Piotr Jałowiecki <jalowieckipiotr@gmail.com>
2018-09-08 13:10:46 +0200

    [#3.2]

--- vue-devmeeting/src/components/ProductList.vue
+++ vue-devmeeting/src/components/ProductList.vue
@@ -2,15 +2,22 @@
 <template>
   <div>
     <ul>
-      <li v-for="p in products" :key="p.id">{{ p.name }}</li>
+      <li v-for="p in products" :key="p.id">
+          <product-list-item :product="p"></product-list-item>
+      </li>
     </ul>
     <p v-if="!products.length">No products!</p>
   </div>
 </template>
 
 <script>
+import ProductListItem from './ProductListItem';
+
 export default {
   name: "ProductList",
+  components:{
+    ProductListItem
+  },
   // The case is, we have to define props (inputs)
   props: {
     // Here we also added some basic props validation
---
+++ vue-devmeeting/src/components/ProductListItem.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div>{product.name}</div>
+</template>
+
+<script>
+export default {
+  name: "ProductListItem",
+  // The case is, we have to define props (inputs)
+  props: {
+    // Here we also added some basic props validation
+    product: {
+      type: Object
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
